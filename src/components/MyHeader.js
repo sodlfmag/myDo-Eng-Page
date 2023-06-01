4d71743ae8e4c5fa8d47ed06211a8f15183225d8
sodlfmag <sodlfmag@khu.ac.kr>
2023-06-01 16:11:36 +0900

    FEAT: 헤더 컴포넌트 구현

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,15 +1,32 @@
+import { Link, NavLink } from "react-router-dom";
+
 export const MyHeader = () => {
   return (
-    <header
-      className="MyHeader"
-      style={{ textAlign: "left", paddingTop: "20px", paddingBottom: "20px" }}
-    >
-      <div>
-        <img
-          src={process.env.PUBLIC_URL + `/assets/images/Header_Logo.svg`}
-          style={{ marginLeft: "2%" }}
-        />
-        <>This is the Header.</>
+    <header className="MyHeader">
+      <div className="HeaderContainer">
+        <NavLink to={"/main"}>
+          <img
+            className="HeaderLogo"
+            src={process.env.PUBLIC_URL + `/assets/images/Header_Logo.svg`}
+          />
+        </NavLink>
+        <div className="LinkContainer">
+          <Link className="HeaderLink" to="/brand">
+            BRAND
+          </Link>
+          <Link className="HeaderLink" to="/ecobottle">
+            ECOBOTTLE
+          </Link>
+          <Link className="HeaderLink" to="/shop">
+            SHOP
+          </Link>
+          <Link className="HeaderLink" to="/contact">
+            CONTACT
+          </Link>
+          <Link className="HeaderLink" to="https://mydo.co.kr/">
+            KOREA
+          </Link>
+        </div>
       </div>
     </header>
   );
