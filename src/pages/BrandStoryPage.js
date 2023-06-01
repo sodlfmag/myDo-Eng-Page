f7e8199d3e79ee8514d755e1c6768922354c699b
sodlfmag <sodlfmag@khu.ac.kr>
2023-06-01 20:55:20 +0900

    FEAT: 브랜드 페이지 샘플 페이지 슬라이드 추가

--- src/pages/BrandStoryPage.js
+++ src/pages/BrandStoryPage.js
@@ -1,7 +1,19 @@
+import { MyHeader } from "../components/MyHeader";
+
 export const BrandStoryPage = () => {
   return (
     <div className="BrandStoryPageContainer">
-      <>This is the BrandStoryPage.</>
+      <img
+        className="PageSlide"
+        src={process.env.PUBLIC_URL + `/assets/images/Brand-Story_1.svg`}
+      />
+      <img
+        className="PageSlide"
+        src={process.env.PUBLIC_URL + `/assets/images/Brand-Story_2.svg`}
+      />
+      <div className="MainHeader">
+        <MyHeader />
+      </div>
     </div>
   );
 };
