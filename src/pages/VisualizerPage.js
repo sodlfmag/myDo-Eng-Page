e8aa2b5acd6b9436fddaa9aa90a4692cf11956db
sodlfmag <sodlfmag@khu.ac.kr>
2023-06-01 13:56:22 +0900

    FEAT: 비주얼라이즈 배경 흰색, 벗어날 시 기본값 변경하는 useEffect 추가

--- src/pages/VisualizerPage.js
+++ src/pages/VisualizerPage.js
@@ -1,24 +1,31 @@
+import { useEffect } from "react";
+
 export const VisualizerPage = () => {
+  // 비주얼라이즈 페이지는 배경을 흰색으로, 벗어나면 기본값으로 돌아감
+  useEffect(() => {
+    document.body.style.backgroundColor = "white";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
   return (
-    <div
-      className="VisualizerContainer"
-      style={{ placeItems: "center", display: "grid" }}
-    >
+    <div className="VisualizerContainer">
       <>
         <video
+          className="NeonMotion"
           autoPlay
           loop
           muted
           src={process.env.PUBLIC_URL + `/assets/videos/NeonMotionx3.mp4`}
-          style={{ width: "50%" }}
         />
       </>
       <>
         <video
+          className="TypoMotion"
           autoPlay
           muted
           src={process.env.PUBLIC_URL + `/assets/videos/TypoMotionx2.mp4`}
-          style={{ width: "50%" }}
         />
       </>
     </div>
