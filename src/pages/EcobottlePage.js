ce90c91f81318b48cbd500841d1d2f395f9718b3
sodlfmag <sodlfmag@khu.ac.kr>
2023-06-01 21:39:39 +0900

    FEAT: 에코보틀 페이지 샘플 및 초기 세팅

--- src/pages/EcobottlePage.js
+++ src/pages/EcobottlePage.js
@@ -1,7 +1,19 @@
+import { MyHeader } from "../components/MyHeader";
+
 export const EcobottlePage = () => {
   return (
     <div className="EcobottlePageContainer">
-      <>This is the EcobottlePage.</>
+      <img
+        className="PageSlide"
+        src={process.env.PUBLIC_URL + `/assets/images/EcoBottle_1.svg`}
+      />
+      <img
+        className="PageSlide"
+        src={process.env.PUBLIC_URL + `/assets/images/EcoBottle_2.svg`}
+      />
+      <div className="MainHeader">
+        <MyHeader />
+      </div>
     </div>
   );
 };
