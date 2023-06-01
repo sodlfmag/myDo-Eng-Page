f1cba83ca90279955fbaad792736b02a663aa560
sodlfmag <sodlfmag@khu.ac.kr>
2023-06-01 12:19:53 +0900

    FEAT: 비주얼라이저 영상 중앙 세팅

--- src/pages/VisualizerPage.js
+++ src/pages/VisualizerPage.js
@@ -1,12 +1,26 @@
 export const VisualizerPage = () => {
   return (
-    <div className="VisualizerContainer">
-      <video
-        autoPlay
-        muted
-        loop
-        src={process.env.PUBLIC_URL + `/assets/videos/Sample_BV_motion.mp4`}
-      />
+    <div
+      className="VisualizerContainer"
+      style={{ placeItems: "center", display: "grid" }}
+    >
+      <>
+        <video
+          autoPlay
+          loop
+          muted
+          src={process.env.PUBLIC_URL + `/assets/videos/NeonMotionx3.mp4`}
+          style={{ width: "50%" }}
+        />
+      </>
+      <>
+        <video
+          autoPlay
+          muted
+          src={process.env.PUBLIC_URL + `/assets/videos/TypoMotionx2.mp4`}
+          style={{ width: "50%" }}
+        />
+      </>
     </div>
   );
 };
