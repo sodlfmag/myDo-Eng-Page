2287b31a2f733cbd8b5d4dcc09f00cdcab429002
sodlfmag <sodlfmag@khu.ac.kr>
2023-10-06 10:50:20 +0900

    FEAT: 모바일 EB 페이지 에셋 추가

--- src/pages/EcobottlePage.js
+++ src/pages/EcobottlePage.js
@@ -1,66 +1,138 @@
 import { PageHeader } from "../components/PageHeader";
 import { MyFooter } from "../components/MyFooter";
 import PageScroll from "react-page-scroll";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 
 export const EcobottlePage = () => {
   return (
     <>
-      <div>
-        <PageHeader name="ECOBOTTLE" />
-      </div>
-      <PageScroll width="100vw" height="100vh">
-        <div
-          id="page1"
-          className="page"
-          style={{
-            backgroundImage: `url(${process.env.PUBLIC_URL + "/EB_bg3.svg"})`,
-            backgroundSize: "cover",
-            height: "100vh",
-          }}
-        >
-          <video
-            muted
-            autoPlay
-            loop
-            src={process.env.PUBLIC_URL + "/PumpingMotion.webm"}
-            height={"100%"}
-          />
-        </div>
-        <div
-          id="page3"
-          className="page"
-          style={{
-            backgroundImage: `url(${process.env.PUBLIC_URL + "/EB_bg1.svg"})`,
-            backgroundSize: "cover",
-            height: "100vh",
-          }}
-        >
-          <img
-            className="EcoBottleSlide"
-            src={process.env.PUBLIC_URL + `/EB_ct1.svg`}
-          />
-        </div>
+      <BrowserView>
         <div>
+          <PageHeader name="ECOBOTTLE" />
+        </div>
+        <PageScroll width="100vw" height="100vh">
           <div
-            id="page2"
+            id="page1"
             className="page"
             style={{
-              backgroundImage: `url(${process.env.PUBLIC_URL + "/EB_bg2.svg"})`,
+              backgroundImage: `url(${process.env.PUBLIC_URL + "/EB_bg3.svg"})`,
+              backgroundSize: "cover",
+              height: "100vh",
+            }}
+          >
+            <video
+              muted
+              autoPlay
+              loop
+              src={process.env.PUBLIC_URL + "/PumpingMotion.webm"}
+              height={"100%"}
+            />
+          </div>
+          <div
+            id="page3"
+            className="page"
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL + "/EB_bg1.svg"})`,
               backgroundSize: "cover",
               height: "100vh",
             }}
           >
             <img
-              className="EcoBottleSlide2"
-              src={process.env.PUBLIC_URL + `/EB_ct2.svg`}
+              className="EcoBottleSlide"
+              src={process.env.PUBLIC_URL + `/EB_ct1.svg`}
             />
           </div>
+          <div>
+            <div
+              id="page2"
+              className="page"
+              style={{
+                backgroundImage: `url(${
+                  process.env.PUBLIC_URL + "/EB_bg2.svg"
+                })`,
+                backgroundSize: "cover",
+                height: "100vh",
+              }}
+            >
+              <img
+                className="EcoBottleSlide2"
+                src={process.env.PUBLIC_URL + `/EB_ct2.svg`}
+              />
+            </div>
+          </div>
+          <div style={{ height: "20vh" }}>
+            <MyFooter />
+          </div>
+          {/* footer 단 상단고정되는 현상 없애기 */}
+        </PageScroll>
+      </BrowserView>
+      <MobileView>
+        <div>
+          <PageHeader name="ECOBOTTLE" />
         </div>
-        <div style={{ height: "20vh" }}>
-          <MyFooter />
-        </div>
-        {/* footer 단 상단고정되는 현상 없애기 */}
-      </PageScroll>
+        <PageScroll width="100vw" height="100vh">
+          {/* <div
+            id="page1"
+            className="page"
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL + "/EB_bg3.svg"})`,
+              backgroundSize: "cover",
+              height: "100vh",
+            }}
+          >
+            <video
+              muted
+              autoPlay
+              loop
+              src={process.env.PUBLIC_URL + "/PumpingMotion.webm"}
+              width={"100%"}
+            />
+          </div> */}
+          <div
+            id="page1"
+            className="page"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/M_EB_bg1.svg"
+              })`,
+              backgroundSize: "cover",
+              height: "100vh",
+            }}
+          >
+            <img
+              className="EcoBottleSlide"
+              src={process.env.PUBLIC_URL + `/M_EB_ct1.svg`}
+            />
+          </div>
+          <div>
+            <div
+              id="page2"
+              className="page"
+              style={{
+                backgroundImage: `url(${
+                  process.env.PUBLIC_URL + "/EM_B_bg2.svg"
+                })`,
+                backgroundSize: "cover",
+                height: "100vh",
+              }}
+            >
+              <img
+                className="EcoBottleSlide2"
+                src={process.env.PUBLIC_URL + `/M_EB_ct2.svg`}
+              />
+            </div>
+          </div>
+          <div style={{ height: "20vh" }}>
+            <MyFooter />
+          </div>
+          {/* footer 단 상단고정되는 현상 없애기 */}
+        </PageScroll>
+      </MobileView>
     </>
   );
 };
